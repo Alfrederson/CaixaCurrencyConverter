f551d9961fdad684b925de1d0522e237efae6e7c
Alfrederson <aroldorosenberg@gmail.com>
2025-09-08 09:16:32 -0300

    começando testes unitarios

--- tests/CurrencyConverterNUnit/CurrencyConverterTests.cs
+++ tests/CurrencyConverterNUnit/CurrencyConverterTests.cs
@@ -1,10 +1,41 @@
 ﻿namespace CurrencyConverterMsTest
 {
+    using CurrencyConverter;
     // TAREFA
     // Testar Conversão A -> B
     // Testar Conversão A -> B, B -> A (colocar na tabela A->B e B->A)
 
+
+    [TestFixture]
     public class CurrencyConverterTests
     {
+        private readonly CurrencyConverter _converter;
+        private readonly RateProvider _provider;
+        public CurrencyConverterTests()
+        {
+            _provider = new RateProvider(new Dictionary<(Currency, Currency), decimal>
+            {
+                {(Currency.USD, Currency.BRL), 2.0m },
+                {(Currency.BRL, Currency.USD), 0.5m}
+            });
+            _converter = new CurrencyConverter(_provider);
+        }
+        [Test]
+        public void ConvertAtoB()
+        {
+            var result = _converter.Convert(new Money(1.0m, Currency.USD), Currency.BRL);
+            Assert.That(result.Amount,Is.EqualTo(2.0m));
+        }
+
+        [Test]
+        public void ConvertAToBToA_ShouldYieldOriginalValue()
+        {
+            var brlPorUsd = _converter.Convert(new Money(1.0m, Currency.USD), Currency.BRL);
+            var usdPorBrl = _converter.Convert(new Money(brlPorUsd.Amount, Currency.BRL), Currency.USD);
+
+            Assert.That(usdPorBrl.Amount,Is.EqualTo(1.0m));
+            Assert.That(usdPorBrl.Currency,Is.EqualTo(Currency.USD));
+        }
+        
     }
 }
